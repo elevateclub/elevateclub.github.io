47549334ca1f0ac0d057bc90e5d5fb577507ebba
elevateclub <126286033+elevateclub@users.noreply.github.com>
2023-07-23 23:31:18 -0400

    whitespace

--- color/script.js
+++ color/script.js
@@ -7,16 +7,16 @@ function updateColor(e) {
   	rinput.valueAsNumber.toString(16).padStart(2, "0") +
   	ginput.valueAsNumber.toString(16).padStart(2, "0") +
   	binput.valueAsNumber.toString(16).padStart(2, "0") + "ff";
-  ctx.fillStyle = val;
-  ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = val;
+    ctx.fillRect(0, 0, width, height);
 }
 
 (function() {
 	rinput = document.getElementById("red");
 	ginput = document.getElementById("green");
-  binput = document.getElementById("blue");
+    binput = document.getElementById("blue");
 	canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  updateColor();
-  [rinput, ginput, binput].forEach((v) => v.addEventListener("change", updateColor));
+    ctx = canvas.getContext("2d");
+    updateColor();
+    [rinput, ginput, binput].forEach((v) => v.addEventListener("change", updateColor));
 })();
