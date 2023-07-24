6a9a1a07f5b27742b4bf15dba118d8aeaf2113aa
elevateclub <126286033+elevateclub@users.noreply.github.com>
2023-07-23 22:54:24 -0400

    enumerate rgba more elegantly

--- color/8-bit/script.js
+++ color/8-bit/script.js
@@ -4,17 +4,17 @@
 
   var v = ["00", "56", "ac", "ff"];
   var i = 0;
-  var r = 0xc0;
-  var g = 0x30;
-  var b = 0x0c;
-  var a = 0x03;
+  var a = 0xc0;
+  var b = 0x30;
+  var g = 0x0c;
+  var r = 0x03;
   var clr = "#00000000";
   for (var x = 0; x < 16; x++) {
   	for (var y = 0; y < 16; y++) {
       ctx.fillStyle = clr;
       ctx.fillRect(x*64, y*64, 64, 64);
 			i++;
-      clr = "#" + v[(i&r)>>6] + v[(i&g)>>4] + v[(i&b)>>2] + v[i&a];
+      clr = "#" + v[(i&r)>>0] + v[(i&g)>>2] + v[(i&b)>>4] + v[(i&a)>>6];
     }
   }
 })();
