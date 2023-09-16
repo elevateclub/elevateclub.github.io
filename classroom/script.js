fc2dbc57e964765d5a22783255e6c656bb8f5ade
Brian Kim <bk@elevateclub.org>
2023-09-16 16:51:33 -0400

    add points ui

--- classroom/script.js
+++ classroom/script.js
@@ -1,12 +1,20 @@
 var students = [];
+var points = [];
+
+function addPoints(i) {
+	points[i]++;
+	$(`#student-point-${i}`).html(`Points: ${points[i]}`)
+}
 
 function addStudent(e) {
 	e.preventDefault();
 	
 	var el = $("#add-student-name-input").val();
 	students.push(el);
+	points.push(0);
 
-	$("#students").append($(`<h1>${el}</h1>`));
+	var n = points.length-1;
+	$("#students").append($(`<h1>${el}</h1><h2 id="student-point-${n}">Points: 0</h2><button onclick="addPoints(${n})" id="student-${el}-add-button">+</button>`));
 }
 
 $('document').ready(function(){
