ed902f553aced2f0f6ddfe151e2e7a8b2a67fc0c
Brian Kim <bk@breadtech.com>
2023-09-28 17:46:07 -0400

    add bin option

--- ascii/script.js
+++ ascii/script.js
@@ -1,19 +1,37 @@
 var textInput;
+var outputTextarea;
+var hexRadio;
+var binRadio;
 
 function convertToHex(str) {
     var hex = '\0';
     for(var i=0;i<str.length;i++) {
-        hex += ''+str.charCodeAt(i).toString(16);
+        hex += ''+str.charCodeAt(i).toString(16).padStart(2, "0");
         hex.length % 24 == 0 ? hex += '\n' : hex += '\t';
     }
     return hex;
 }
 
+function convertToBin(str) {
+    var bin = '';
+    for(var i=0;i<str.length;i++) {
+        bin += ''+str.charCodeAt(i).toString(2).padStart(8, "0") +'\n';
+    }
+    return bin;
+}
+
 function updateTextArea(e) {
-    document.getElementById("hex-textarea").innerHTML = convertToHex(textInput.value);
+    var output = hexRadio.checked ? convertToHex(textInput.value) : convertToBin(textInput.value);
+    outputTextarea.innerHTML = output;
 }
 
 (function() {
-    textInput = document.getElementById("text-input");
+    textInput = document.querySelector("#input");
+    outputTextarea = document.querySelector("#output-textarea");
+    hexRadio = document.querySelector("#hex-radio");
+    binRadio = document.querySelector("#bin-radio");
+
     textInput.addEventListener("change", updateTextArea);
+    hexRadio.addEventListener("change", updateTextArea)
+    binRadio.addEventListener("change", updateTextArea)
 })();
